65e1bd9d50686ea967f39c9abbf7d696031da4ec
leszekwawrzonkowski <leszekwawrzonkowski@users.noreply.github.com>
2021-12-08 21:05:36 +0100

    day two

--- src/aoc2021.ts
+++ src/aoc2021.ts
@@ -1,4 +1,4 @@
-type PuzzleNumber = "D1P1" | "D1P2" | "D2P2" | "D2P2";
+type PuzzleNumber = "D1P1" | "D1P2" | "D2P1" | "D2P2";
 
 function solvePuzzle(puzzleText: string, puzzleNumber: PuzzleNumber): string {
     switch (puzzleNumber) {
@@ -23,6 +23,85 @@ function solvePuzzle(puzzleText: string, puzzleNumber: PuzzleNumber): string {
             }
             return String(numberOfIncreases);
         }
+        case "D2P1":
+            // Calculate the horizontal position and depth you would have after following the planned course. 
+            // What do you get if you multiply your final horizontal position by your final depth?
+            {
+                class SubmarinePosition {
+                    horizontal: number;
+                    depth: number;
+
+                    constructor() {
+                        this.horizontal = 0;
+                        this.depth = 0;
+                    }
+
+                    changePosition(commandAndUnit: string) {
+                        let commandAndUnitElements = commandAndUnit.split(" ");
+                        const command = commandAndUnitElements[0];
+                        let unit = Number(commandAndUnitElements[1]);
+                        switch (command) {
+                            case "forward":
+                                this.horizontal += unit;
+                                break;
+                            case "down":
+                                this.depth += unit;
+                                break;
+                            case "up":
+                                this.depth -= unit;
+                                break;
+                        }
+                    }
+                }
+
+                let puzzleArray = puzzleInputToStringArray(puzzleText);
+                let submarinePosition = new SubmarinePosition();
+                puzzleArray.forEach(puzzle => {
+                    submarinePosition.changePosition(puzzle);
+                });
+                return String(submarinePosition.depth * submarinePosition.horizontal);
+            }
+            case "D2P2":
+                // Calculate the horizontal position and depth you would have after following the planned course. 
+                // What do you get if you multiply your final horizontal position by your final depth?
+                {
+                    class SubmarinePosition {
+                        horizontal: number;
+                        depth: number;
+                        aim: number;
+    
+                        constructor() {
+                            this.horizontal = 0;
+                            this.depth = 0;
+                            this.aim = 0;
+                        }
+    
+                        changePosition(commandAndUnit: string) {
+                            let commandAndUnitElements = commandAndUnit.split(" ");
+                            const command = commandAndUnitElements[0];
+                            let unit = Number(commandAndUnitElements[1]);
+                            switch (command) {
+                                case "forward":
+                                    this.horizontal += unit;
+                                    this.depth += (this.aim * unit);
+                                    break;
+                                case "down":
+                                    this.aim += unit;
+                                    break;
+                                case "up":
+                                    this.aim -= unit;
+                                    break;
+                            }
+                        }
+                    }
+    
+                    let puzzleArray = puzzleInputToStringArray(puzzleText);
+                    let submarinePosition = new SubmarinePosition();
+                    puzzleArray.forEach(puzzle => {
+                        submarinePosition.changePosition(puzzle);
+                    });
+                    return String(submarinePosition.depth * submarinePosition.horizontal);
+                }
     }
 
     return "not yet solved !!!";
--- target/js/aoc2021.js
+++ target/js/aoc2021.js
@@ -1,6 +1,7 @@
 function solvePuzzle(puzzleText, puzzleNumber) {
     switch (puzzleNumber) {
         case "D1P1":
+            // How many measurements are larger than the previous measurement?
             {
                 var puzzleArray = puzzleInputToNumberArray(puzzleText);
                 var numberOfIncreases = 0;
@@ -11,14 +12,86 @@ function solvePuzzle(puzzleText, puzzleNumber) {
                 return String(numberOfIncreases);
             }
         case "D1P2":
+            // Count the number of times the sum of measurements in this sliding three-measurement window increases from the previous sum
             {
                 var puzzleArray = puzzleInputToNumberArray(puzzleText);
                 var numberOfIncreases = 0;
+                // TODO: think about more elegant implementation of the "sliding window" check
                 for (var i = 3; i < puzzleArray.length; i++) {
                     if ((puzzleArray[i - 2] + puzzleArray[i - 1] + puzzleArray[i]) > ((puzzleArray[i - 3] + puzzleArray[i - 2] + puzzleArray[i - 1])))
                         numberOfIncreases++;
                 }
                 return String(numberOfIncreases);
+            }
+        case "D2P1":
+            // Calculate the horizontal position and depth you would have after following the planned course. 
+            // What do you get if you multiply your final horizontal position by your final depth?
+            {
+                var SubmarinePosition = /** @class */ (function () {
+                    function SubmarinePosition() {
+                        this.horizontal = 0;
+                        this.depth = 0;
+                    }
+                    SubmarinePosition.prototype.changePosition = function (commandAndUnit) {
+                        var commandAndUnitElements = commandAndUnit.split(" ");
+                        var command = commandAndUnitElements[0];
+                        var unit = Number(commandAndUnitElements[1]);
+                        switch (command) {
+                            case "forward":
+                                this.horizontal += unit;
+                                break;
+                            case "down":
+                                this.depth += unit;
+                                break;
+                            case "up":
+                                this.depth -= unit;
+                                break;
+                        }
+                    };
+                    return SubmarinePosition;
+                }());
+                var puzzleArray = puzzleInputToStringArray(puzzleText);
+                var submarinePosition_1 = new SubmarinePosition();
+                puzzleArray.forEach(function (puzzle) {
+                    submarinePosition_1.changePosition(puzzle);
+                });
+                return String(submarinePosition_1.depth * submarinePosition_1.horizontal);
+            }
+        case "D2P2":
+            // Calculate the horizontal position and depth you would have after following the planned course. 
+            // What do you get if you multiply your final horizontal position by your final depth?
+            {
+                var SubmarinePosition = /** @class */ (function () {
+                    function SubmarinePosition() {
+                        this.horizontal = 0;
+                        this.depth = 0;
+                        this.aim = 0;
+                    }
+                    SubmarinePosition.prototype.changePosition = function (commandAndUnit) {
+                        var commandAndUnitElements = commandAndUnit.split(" ");
+                        var command = commandAndUnitElements[0];
+                        var unit = Number(commandAndUnitElements[1]);
+                        switch (command) {
+                            case "forward":
+                                this.horizontal += unit;
+                                this.depth += (this.aim * unit);
+                                break;
+                            case "down":
+                                this.aim += unit;
+                                break;
+                            case "up":
+                                this.aim -= unit;
+                                break;
+                        }
+                    };
+                    return SubmarinePosition;
+                }());
+                var puzzleArray = puzzleInputToStringArray(puzzleText);
+                var submarinePosition_2 = new SubmarinePosition();
+                puzzleArray.forEach(function (puzzle) {
+                    submarinePosition_2.changePosition(puzzle);
+                });
+                return String(submarinePosition_2.depth * submarinePosition_2.horizontal);
             }
     }
     return "not yet solved !!!";
@@ -29,3 +102,16 @@ function puzzleInputToStringArray(puzzleInput) {
 function puzzleInputToNumberArray(puzzleInput) {
     return puzzleInputToStringArray(puzzleInput).map(Number);
 }
+/*
+enum PuzzleOutputArrayType {
+    String,
+    Number,
+}
+function puzzleInputToArray(puzzleInput:string, puzzleOutputArrayType: PuzzleOutputArrayType): string[] | number[] {
+    let puzzleElements = puzzleInput.trim().split(/\r?\n/);
+    if (puzzleOutputArrayType === PuzzleOutputArrayType.String) {
+        return puzzleElements.map(Number);
+    }
+    puzzleElements;
+}
+*/ 
